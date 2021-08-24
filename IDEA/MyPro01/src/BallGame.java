55615d9af39bc116af80847ffcd5938972f8a432
zj970 <3060529292@qq.com>
2021-08-24 20:39:38 +0800

    SpringBoot_Web员工管理界面首页面配置，国际化

--- IDEA/MyPro01/src/BallGame.java
+++ IDEA/MyPro01/src/BallGame.java
@@ -1,30 +1,26 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class BallGame extends JFrame
-{
+public class BallGame extends JFrame {
     Image ball = Toolkit.getDefaultToolkit().getImage("image/ball.png");
     Image desk = Toolkit.getDefaultToolkit().getImage("image/desk.jpg");
 
     //画窗口的方法  会有懒加载问题
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g) {
         System.out.println("窗口被画了一次");
-        g.drawImage(desk,0,0,null);
-        g.drawImage(ball,100,100,null);
+        g.drawImage(desk, 0, 0, null);
+        g.drawImage(ball, 100, 100, null);
 
     }
 
     //窗口加载
-    void LaunchFrame()
-    {
-        setSize(300,500);
-        setLocation(50,50);
+    void LaunchFrame() {
+        setSize(300, 500);
+        setLocation(50, 50);
         setVisible(true);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         BallGame game = new BallGame();
         game.LaunchFrame();
         //game.Paint();
