17df5a4b6808e89648adb05dcd42b7cdd75b5de4
zj970 <3060529292@qq.com>
2021-08-25 13:31:41 +0800

    SpringBoot_Web员工管理_登录+拦截器

--- IDEA/StaffingSystem/src/main/java/com/staffingsystem/config/MyMvcConfig.java
+++ IDEA/StaffingSystem/src/main/java/com/staffingsystem/config/MyMvcConfig.java
@@ -3,6 +3,7 @@ package com.staffingsystem.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,12 +16,23 @@ public class MyMvcConfig implements WebMvcConfigurer {
         WebMvcConfigurer.super.addViewControllers(registry);
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboard");//映射到
 
     }
 
-    //自定义的国际化组件
+    //自定义的国际化组件 加载本地的解析器
     @Bean
-    public LocaleResolver localeResolver(){
+    public LocaleResolver localeResolver() {
         return new MyLocaleReolver();
+    }
+
+    //拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+        //addPathPatterns拦截页面 excludePathPatterns忽略拦截页面
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")//增加拦截页面
+                .excludePathPatterns("/index.html", "/", "/user/login","/css/*","/js/*","/img/*");//忽略拦截页面
     }
 }
