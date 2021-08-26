aaab38026520495f6fc36df9e1a4a81435f4a5f6
zj970 <3060529292@qq.com>
2021-08-26 09:48:18 +0800

    SpringBoot_Web员工管理_显示，数据前后端传递，增加

---
+++ IDEA/StaffingSystem/src/main/java/com/staffingsystem/controller/EmployeeController.java
@@ -0,0 +1,48 @@
+package com.staffingsystem.controller;
+
+import com.staffingsystem.dao.DepartmentDao;
+import com.staffingsystem.dao.EmployeeDao;
+import com.staffingsystem.pojo.Department;
+import com.staffingsystem.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Collection;
+
+@Component
+@Controller
+public class EmployeeController {
+
+    @Autowired //注入
+    EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao department;
+    //自定义路径
+    @RequestMapping("/emps")
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getAll();//获得所有员工信息
+        //System.out.println("==---------"+employees);
+        model.addAttribute("employees",employees);//返回给前端
+        return "emp/list";//返回到这个页面
+    }
+
+    @GetMapping("/toAdd")
+    public String toAddpage(Model model){
+        //查出所有部门的信息
+        Collection<Department> departments = department.getDepartments();
+        model.addAttribute("departmentName",departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/toAdd")
+    public String addEmp(Employee employee){
+        //post请求添加信息 forward
+        System.out.println("==---------"+employee);
+        employeeDao.Save(employee);//调用底层业务方法保存员工信息
+        return "redirect:/emps";
+    }
+}
