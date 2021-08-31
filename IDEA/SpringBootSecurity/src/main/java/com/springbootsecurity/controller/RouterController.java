13c7c31bf41400bd640a3a081defcfcd2e3aed87
zj970 <3060529292@qq.com>
2021-08-31 13:21:31 +0800

    SpringBootSecurity项目

---
+++ IDEA/SpringBootSecurity/src/main/java/com/springbootsecurity/controller/RouterController.java
@@ -0,0 +1,7 @@
+package com.springbootsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RouterController {
+}
---
+++ IDEA/SpringBootSecurity/src/test/java/com/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
