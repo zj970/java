60499999fe152fbf5fe8c9546860e37fb6c8fe6e
zj970 <3060529292@qq.com>
2021-09-01 15:06:53 +0800

    SpringBootSecurity项目_简单登录，权限认证，thymeleaf整合sercurity

--- IDEA/SpringBootSecurity/src/main/java/com/springbootsecurity/config/SecurityConfig.java
+++ IDEA/SpringBootSecurity/src/main/java/com/springbootsecurity/config/SecurityConfig.java
@@ -4,22 +4,25 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.servlet.http.HttpSession;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    //认证
+    //认证，springboot 2.1.x 可以直接使用
+    //报错500，id="null" 密码编码
+    //在Spring Secutiry 5.0+新增了很多的加密方法
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //这些数据正常应该从数据库中读取
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("admin").roles("vip2","vip3")
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("admin").password(new BCryptPasswordEncoder().encode("admin")).roles("vip2","vip3")
                 .and()
-                .withUser("root").password("root").roles("vip1","vip2","vip3")
+                .withUser("root").password(new BCryptPasswordEncoder().encode("root")).roles("vip1","vip2","vip3")
                 .and()
-                .withUser("zj").password("123456").roles("vip1");
+                .withUser("zj").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1");
     }
 
     //链式编程 基于建造者模式
@@ -35,8 +38,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         //没有权限默认到登录页面
-        http.formLogin();
-
+        http.formLogin().loginPage("/usr/login");
+        //防止网站攻击：get,post
+        http.csrf().disable();//关闭csrf功能，防止跨站请求伪造关闭，慎用！登录注销失败使用
+        //注销功能开启，跳到首页
+        http.logout().logoutSuccessUrl("/");
 
 
     }
