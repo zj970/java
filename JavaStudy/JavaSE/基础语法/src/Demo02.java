49b665ff2acdd4117d90171826bd97de26c6fc12
zj970 <3060529292@qq.com>
2021-09-14 21:49:00 +0800

    2021-9-14学习java打卡

---
+++ JavaStudy/JavaSE/基础语法/src/Demo02.java
@@ -0,0 +1,34 @@
+public class Demo02 {
+    public static void main(String[] args){
+        //八大基本数据类型
+
+        //整数
+        int num1 = 20;//对应类Integer
+        Integer Num1;
+        byte num2 = 127;
+        Byte Num2;
+        short num3 =23;
+        Short Num3;
+        long num4 = 30L;
+        Long Num4;
+
+        //浮点数
+        float num5 = 50.2f;
+        double num6 = 3.156749818978979798d;
+
+        //字符
+        char name7 = 'A';//用单引号
+        //字符串，Striing是一个类
+        //String name = "dscfdsf";
+
+        //b布尔值
+        boolean nmae8 = false;
+
+
+        String a = "hello";
+        int num = 10;
+        System.out.println(num);
+        System.out.println(a);
+
+    }
+}
