8b4eb488a0c4995d99716d308fec8334abacd68e
zj970 <3060529292@qq.com>
2021-09-18 21:58:30 +0800

    2021-9-18-java学习打卡

---
+++ JavaStudy/JavaSE/基础语法/src/Demo09.java
@@ -0,0 +1,9 @@
+public class Demo09 {
+
+    //修饰符， 不存在先后顺序
+    static  final double PI = 3.14;//C语言中用const
+    
+    public static void main(String[] args) {
+        System.out.println(PI);
+    }
+}
