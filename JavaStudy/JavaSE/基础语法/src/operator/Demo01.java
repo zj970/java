267eaecb125488efbdf673319750ef3c5b967ffc
zj970 <3060529292@qq.com>
2021-09-22 22:15:44 +0800

    2021-9-22-java学习打卡

---
+++ JavaStudy/JavaSE/基础语法/src/operator/Demo01.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class Demo01 {
+    public static void main(String[] args){
+        //二元运算符
+        //ctrl+ D :复制到下一行
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / (double) b);
+    }
+}
---
+++ JavaStudy/JavaSE/基础语法/src/operator/Demo03.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class Demo03 {
+    public static void main(String[] args) {
+        //关系运算符返回的结果：true / false 布尔值
+
+        int a = 10;
+        int b = 20;
+        int c = 21;
+
+        System.out.println(c % a); //1取余
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
