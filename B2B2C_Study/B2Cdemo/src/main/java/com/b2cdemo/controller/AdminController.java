4594f9c52c54483538497c98744b186a05c83a22
zj970 <3060529292@qq.com>
2021-09-29 11:54:11 +0800

    2021-9-29 b2cdemo SpringBoot+Mybaits

--- B2B2C_Study/B2Cdemo/src/main/java/com/b2cdemo/controller/AdminController.java
+++ B2B2C_Study/B2Cdemo/src/main/java/com/b2cdemo/controller/AdminController.java
@@ -12,12 +12,11 @@ import java.util.List;
 @RestController
 public class AdminController {
 
-
     @Autowired
     private AdminMapper adminMapper;
 
     //查询所有用户
-    @GetMapping("/queryAdminList")
+    @GetMapping ("/queryAdminList")
     public List<Admin> queryAdminList(){
         List<Admin> adminList = adminMapper.queryAdminList();
         for (Admin admin:adminList){
@@ -28,23 +27,31 @@ public class AdminController {
 
     //查询单个用户
     @GetMapping("/seletone/{id}")
-    public Admin queryAdmin(@PathVariable("id") Integer id){
+    public Admin queryAdmin(@PathVariable("id")Integer id){
         Admin admin = adminMapper.queryAdminById(id);
+        System.out.println("ok");
         return admin;
+    }
+
+    //添加用户
+    @GetMapping("/addAdmin")
+    public String addAdmin(){
+        System.out.println("ok");
+        return "ok";
     }
 
     //修改用户
     @GetMapping("/updateAdmin/{id}")
-    public String updateAdmin(@PathVariable("id") int id){
-        adminMapper.updateAdmin(new Admin(id,"韩信","123456"));
+    public String updateAdmin(@PathVariable("id")Integer id){
+        System.out.println("ok");
         return "ok";
     }
 
     //删除用户
     @GetMapping("/deleteAdmin/{id}")
-    public String deleteAdmin(@PathVariable("id") Integer id){
-        adminMapper.deleteAdmin(id);
+    public String deleteAdmin(@PathVariable("id")Integer id){
+        System.out.println("ok");
         return "ok";
     }
-
 }
+
