f4f08c5359dcd5fcd5fb02e6d7da1d3c6f151ca1
zj970 <3060529292@qq.com>
2021-10-10 21:58:51 +0800

    2021-10-10学习java打卡java方法

---
+++ JavaStudy/JavaSE/基础语法/src/method/Demo04.java
@@ -0,0 +1,17 @@
+package method;
+
+public class Demo04 {
+    public static void main(String[] args) {
+
+    }
+
+    public void method(){}
+    public void method(int i){}
+    public void method(int i, int j){}
+    public void method(double i, int j){}
+    public void method(double i, double j){}
+
+    //不定参数 一个方法只能指定一个参数
+    public void method(double...i){}
+
+}
