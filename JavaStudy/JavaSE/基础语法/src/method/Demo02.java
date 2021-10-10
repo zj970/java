bbcb49fe0677cb3c379a8748e354de746c9a4d2e
zj970 <3060529292@qq.com>
2021-10-10 20:58:27 +0800

    2021-10-10学习java打卡java方法

--- JavaStudy/JavaSE/基础语法/src/method/Demo02.java
+++ JavaStudy/JavaSE/基础语法/src/method/Demo02.java
@@ -11,4 +11,13 @@ public class Demo02 {
         return num1 > num2 ? num1 : num2;
     }
     //return 0;//终止方法
+
+    //方法重载
+    public static double max(double num1, double num2) {
+        return num1 > num2 ? num1 : num2;
+    }
+    /*仅仅返回类型不同不足以成为方法的重载。
+    public static double max(int num1, int num2) {
+        return num1 > num2 ? num1 : num2;
+    }*/
 }
