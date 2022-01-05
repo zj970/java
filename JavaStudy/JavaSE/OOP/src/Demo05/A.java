725effb49b16b01b2c50cc271950c922a0cf0cd1
zj970 <3060529292@qq.com>
2022-01-05 20:51:20 +0800

    2022-1-4 java学习

---
+++ JavaStudy/JavaSE/OOP/src/Demo05/A.java
@@ -0,0 +1,11 @@
+package Demo05;
+
+//重写都是方法的重写，和属性无关
+public class A {
+    public void test()
+    {
+        System.out.println("A=>test()");
+    }
+
+
+}
---
+++ JavaStudy/JavaSE/OOP/src/Demo05/B.java
@@ -0,0 +1,15 @@
+package Demo05;
+
+public class B extends A{
+//    public static void test()
+//    {
+//        System.out.println("B=>test()");
+//    }
+
+
+    //Override 重写
+    @Override//注解，有功能的注释
+    public void test() {
+        System.out.println("B=>test()");
+    }
+}
