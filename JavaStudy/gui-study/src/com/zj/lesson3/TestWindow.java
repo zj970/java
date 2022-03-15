63927f5b43639656ddae88bd8fcf984e490a88c7
zj970 <3060529292@qq.com>
2022-03-15 22:33:38 +0800

    2022-3-15 java学习

--- JavaStudy/gui-study/src/com/zj/lesson3/TestWindow.java
+++ JavaStudy/gui-study/src/com/zj/lesson3/TestWindow.java
@@ -1,6 +1,28 @@
 package com.zj.lesson3;
 
 
-public class TestWindow {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+public class TestWindow {
+    public static void main(String[] args) {
+        new WindowFrame();
+    }
+}
+
+class WindowFrame extends Frame {
+    public WindowFrame(){
+        setBackground(Color.blue);
+        setBounds(100,100,200,200);
+        setVisible(true);
+        addWindowListener(new MyWindowListener());
+    }
+    class MyWindowListener extends WindowAdapter{
+        @Override
+        public void windowClosing(WindowEvent e) {
+            setVisible(false);//隐藏窗口，通过按钮
+            System.exit(0);
+        }
+    }
 }
