800e50851deaff08ec47abd5a708c218130fc7c2
zj970 <3060529292@qq.com>
2022-03-16 22:47:33 +0800

    2022-3-16 java学习

--- JavaStudy/gui-study/src/com/zj/lesson3/TestWindow.java
+++ JavaStudy/gui-study/src/com/zj/lesson3/TestWindow.java
@@ -34,10 +34,20 @@ class WindowFrame extends Frame {
         setBounds(100,100,200,200);
         setVisible(true);
         addWindowListener(new WindowAdapter() {
+            //匿名内部类
+            //关闭窗口
             @Override
             public void windowClosing(WindowEvent e) {
                 setVisible(false);//隐藏窗口，通过按钮
                 System.exit(0);
+            }
+            //激活窗口
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+                WindowFrame source = (WindowFrame) e.getSource();
+                source.setTitle("被激活了");
+                System.out.println("windowActivated");
             }
         });
     }
