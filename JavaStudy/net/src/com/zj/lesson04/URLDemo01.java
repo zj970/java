5cf634f4397e6d161f10aca4772c40939a3b27a6
zj970 <3060529292@qq.com>
2022-06-21 22:50:55 +0800

    [add]: URLDemo01.java URLDown.java [update] java学习.md [shows] 学习如何是用URL 下载文件

---
+++ JavaStudy/net/src/com/zj/lesson04/URLDemo01.java
@@ -0,0 +1,17 @@
+package com.zj.lesson04;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLDemo01 {
+    public static void main(String[] args) throws MalformedURLException {
+        URL url = new URL("http://localhost:8080/helloworld/index.jsp?username=zj&password=123");
+        System.out.println(url.getProtocol());//获得协议名
+        System.out.println(url.getHost());//主机ip
+        System.out.println(url.getPort());//端口
+        System.out.println(url.getPath());//文件
+        System.out.println(url.getFile());//全路径
+        System.out.println(url.getQuery());//参数
+
+    }
+}
---
+++ JavaStudy/net/src/com/zj/lesson04/URLDown.java
@@ -0,0 +1,29 @@
+package com.zj.lesson04;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLDown {
+    public static void main(String[] args) throws Exception {
+        //1. 下载地址
+        URL url = new URL("http://m701.music.126.net/20220621231213/b1089356069f998e45dcdd5327352191/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/14960576886/cfd4/fb18/3885/f23678ff0e30fbda7924d2957acccb57.m4a");
+
+        //2. 连接这个资源 HTTP
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        InputStream inputStream = urlConnection.getInputStream();
+        FileOutputStream fos = new FileOutputStream("ss.m4a");
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = inputStream.read(buffer)) != -1){
+            fos.write(buffer,0,len);//写出这个数据
+        }
+
+        fos.close();
+        inputStream.close();
+        urlConnection.connect();//断开连接
+    }
+}
